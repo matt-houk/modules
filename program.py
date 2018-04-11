479fc0188a2f2d8f9d032afbd048453d906e0804
Adam Cogdell <1231862.student@gmail.com>
2018-04-11 10:12:58 -0400

    Update program.py

--- program.py
+++ program.py
@@ -6,3 +6,17 @@ print(pythag.hypotenuse( ))
 
 print(pythag.leg( ))
 
+# int2word module - Adam
+from int2word import wordify
+
+for i in range(0, 11):
+    print(wordify(i))
+
+for i in range(110, 122):
+    print(wordify(i))
+
+for i in range(999999990, 1000000000):
+    print(wordify(i))
+
+for i in range(0, 1000000000):
+    print(wordify(i))
