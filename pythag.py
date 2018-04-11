824f849e1f4f4bd3bd9ab7a5c2ed738fa22c69b9
overpeck19c <38252850+overpeck19c@users.noreply.github.com>
2018-04-11 10:15:56 -0400

    Update pythag.py

--- pythag.py
+++ pythag.py
@@ -1,5 +1,20 @@
-#module for finding hypotenuse and a 
-#leg of a right triangle using pythagorean theorem
+###########################################################################
+#   Author:  Chase Overpeck                                               #
+#      PID:  99s380241                                                    #
+#    Class:  PH412, Spring, 2018                                          #
+#  Helpers:  n/a                                                          #
+#                                                                         #
+#  Program:  Modules Assignment                                           #
+# Due Date:  April 12, 2018                                               #
+#                                                                         #
+# Language:  Python 2.7                                                   #
+#      IDE:  Trinket                                                      #
+#                                                                         #
+#  Purpose:  Module for finding hypotenuse and a leg of a right triangle  #
+#            using pythagorean theorem                                    #
+#                                                                         #
+#   "Bugs":  No major problems                                            #
+###########################################################################
 
 def hypotenuse(a, b):
     c = (a**2 + b**2)**0.5
