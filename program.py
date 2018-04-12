d62367b90fe8345a3b1e8229d41f29c84643d1c3
matt-houk <32654145+matt-houk@users.noreply.github.com>
2018-04-12 01:53:16 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,7 @@
 #running the pythag module - chase
 
+print('\n Testing pythag.py \n')
+
 import pythag
 
 print(pythag.hypotenuse(3, 4))
@@ -7,6 +9,9 @@ print(pythag.hypotenuse(3, 4))
 print(pythag.leg(4, 5))
 
 # int2word module - Adam
+
+print('\n Testing int2word.py \n')
+
 from int2word import wordify
 
 for i in range(0, 11):
@@ -20,6 +25,9 @@ for i in range(999999990, 1000000000):
 
 
 # Running the encryption module:
+
+print('\n Testing encryption.py \n')
+
 from encryption import encryptblock, decryptblock
 
 string = 'Hello World!'
@@ -41,6 +49,9 @@ if decrypted == string:
     print("The string was properly encrypted and decrypted")
 
 # Running the sequences module - william
+
+print('\n Testing sequences.py \n')
+
 import sequences
 
 print("Partial sum of 1/10**n to 5th element:", sequences.partial_sum(5, 0, "1/10**n"))
